06d1b721eaae051f8552af5d6183b5097d8ed67e
Kamil Solanki <kamilsolanki99@gmail.com>
2025-06-29 15:25:28 +0530

    Updated project with new code

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def launch_browser():
 
     url = "https://www.shcilestamp.com/eStampIndia/useradmin/UserAdminLoginServlet?rDoAction=LoadLoginPage"
     user_id = "gjdpipati"
-    password = "Ddpkm$2025"
+    password = "Ddpkm@$2025"
 
     try:
         driver = setup_driver()
